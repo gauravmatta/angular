8068f63338087666f080e98601aa4cc3626280c9
Gaurav Matta <gaurav.matta@gmail.com>
2020-07-06 00:30:16 +0530

    Cleaned Asyc Pipe

--- async-pipe/main.ts
+++ async-pipe/main.ts
@@ -1,55 +1,17 @@
 import {Component, Input, Pipe} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-import {DatePipe} from 'angular2/src/common/pipes';
-
-@Pipe({
-  name: 'pixelPipe'
-})
-class SimplePipe {
-  transform(input){
-    return input + 'px';
-  }
-}
-
-@Pipe({
-  name: 'unit'
-})
-class UnitPipe {
-  transform(input,arg1,arg2){
-    let unit = arg1 || 'px';
-    arg2 = arg2 || '5';
-    return input + unit + arg2;
-  }
-}
 
 /**
  * Define the Component.
  */
 @Component({
   selector: 'app',
-  templateUrl : 'templates/app.tpl.html',
-  pipes:[SimplePipe,UnitPipe]
+  templateUrl : 'templates/app.tpl.html'
 })
 class StarterTemplate  {
   private name: string;
-  private value: string;
-  private today: Date;
-  private todayInside;
-  private students:any[];
   constructor () {
-    const unitPipe = new UnitPipe();
-    const datePipe = new DatePipe();
-    this.name = 'Basic Pipe Example';
-    this.value= unitPipe.transform('12','%','10');
-    this.today=new Date();
-    this.todayInside=datePipe.transform(new Date(),'MMMh');
-    this.students=[
-      {id:1, name:'Tom'},
-      {id:2, name:'Jerry'},
-      {id:3, name:'Goofy'},
-      {id:4, name:'Flinstone'},
-      {id:5, name:'Shabby'}
-    ];
+    this.name = 'Async Pipe Example';
   }
 }
 
--- async-pipe/output/main.js
+++ async-pipe/output/main.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core", "angular2/platform/browser", "angular2/src/common/pipes"], function (exports_1, context_1) {
+System.register(["angular2/core", "angular2/platform/browser"], function (exports_1, context_1) {
     "use strict";
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -9,7 +9,7 @@ System.register(["angular2/core", "angular2/platform/browser", "angular2/src/com
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, browser_1, pipes_1, SimplePipe, UnitPipe, StarterTemplate;
+    var core_1, browser_1, StarterTemplate;
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [
@@ -18,61 +18,17 @@ System.register(["angular2/core", "angular2/platform/browser", "angular2/src/com
             },
             function (browser_1_1) {
                 browser_1 = browser_1_1;
-            },
-            function (pipes_1_1) {
-                pipes_1 = pipes_1_1;
             }
         ],
         execute: function () {
-            SimplePipe = /** @class */ (function () {
-                function SimplePipe() {
-                }
-                SimplePipe.prototype.transform = function (input) {
-                    return input + 'px';
-                };
-                SimplePipe = __decorate([
-                    core_1.Pipe({
-                        name: 'pixelPipe'
-                    })
-                ], SimplePipe);
-                return SimplePipe;
-            }());
-            UnitPipe = /** @class */ (function () {
-                function UnitPipe() {
-                }
-                UnitPipe.prototype.transform = function (input, arg1, arg2) {
-                    var unit = arg1 || 'px';
-                    arg2 = arg2 || '5';
-                    return input + unit + arg2;
-                };
-                UnitPipe = __decorate([
-                    core_1.Pipe({
-                        name: 'unit'
-                    })
-                ], UnitPipe);
-                return UnitPipe;
-            }());
             StarterTemplate = /** @class */ (function () {
                 function StarterTemplate() {
-                    var unitPipe = new UnitPipe();
-                    var datePipe = new pipes_1.DatePipe();
-                    this.name = 'Basic Pipe Example';
-                    this.value = unitPipe.transform('12', '%', '10');
-                    this.today = new Date();
-                    this.todayInside = datePipe.transform(new Date(), 'MMMh');
-                    this.students = [
-                        { id: 1, name: 'Tom' },
-                        { id: 2, name: 'Jerry' },
-                        { id: 3, name: 'Goofy' },
-                        { id: 4, name: 'Flinstone' },
-                        { id: 5, name: 'Shabby' }
-                    ];
+                    this.name = 'Async Pipe Example';
                 }
                 StarterTemplate = __decorate([
                     core_1.Component({
                         selector: 'app',
-                        templateUrl: 'templates/app.tpl.html',
-                        pipes: [SimplePipe, UnitPipe]
+                        templateUrl: 'templates/app.tpl.html'
                     }),
                     __metadata("design:paramtypes", [])
                 ], StarterTemplate);
